db682b7b2476d54512caddd864388da2b450a93c
mikethevrguy <40547376+mikethevrguy@users.noreply.github.com>
2022-10-21 10:49:03 -0600

    Recipe Commit for Trinh

---
+++ Assets/Scripts/RecipeExample/PanLogic.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PanLogic : MonoBehaviour
+{
+    [SerializeField]
+    RecipeBook recipeBook;
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+    private int min;
+    private int max;
+    float timer = 0;
+    private void OnMouseDown()
+    {
+        StopAllCoroutines();
+        
+        if (timer < min && timer !=0)
+        {
+            Debug.Log("Food is undercooked!");
+            Debug.Log("You lost $" + recipeBook.CalculateEarnings(false));
+            Debug.Log("Total profit $" + recipeBook.earnings);
+            scoreText.text = "Earnings: $" + recipeBook.earnings;
+        } else if (timer > max && timer != 0)
+        {
+            Debug.Log("Food is burned!");
+            Debug.Log("You lost $" + recipeBook.CalculateEarnings(false));
+            Debug.Log("Total profit $" + recipeBook.earnings);
+            scoreText.text = "Earnings: $" + recipeBook.earnings;
+        } else if (timer != 0)
+        {
+            Debug.Log("Food is cooked perfectly!");
+            Debug.Log("You earned $" + recipeBook.CalculateEarnings(true));
+            Debug.Log("Total profit $" + recipeBook.earnings);
+            scoreText.text = "Earnings: $" + recipeBook.earnings;
+        }
+        timer = 0;
+        
+
+        recipeBook.ChooseRecipe();
+    }
+    public void StartCountDown(int _min, int _max)
+    {
+        min = _min;
+        max = _max;
+        StartCoroutine(Timer());
+    }
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            timer += Time.deltaTime;
+            Debug.Log(timer);
+            yield return null;
+        }
+    }
+}
